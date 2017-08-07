5c8e19d4bff7893ba1650fc0ce5cd6da85068016
tawfeh <sennegueye@gmail.com>
2017-08-07 09:41:28 +0000

    Add All files

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+int a,b(0),c;
+;
+
+    cout << "saisir un nombre " << endl;
+     cin >> a;
+     while (b==0){
+     cout << "saisir un autre nombre " << endl;
+     cin >> b;
+     }
+
+     c= a % b;
+    cout <<"le reste de la division de "<< a << " et de " << b << " = "<< c <<endl;
+       return 0;
+
+}
+
+
